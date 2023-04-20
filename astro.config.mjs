470ba77c552864646f27ecb520dbce69382fd88f
Juan Vistro <jpmbvistro@gmail.com>
2023-04-20 10:35:39 -0400

    add logos and header lightmode style changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,27 @@
-import { defineConfig } from 'astro/config'
-import compress from 'astro-compress'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import compress from 'astro-compress';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import storyblok from "@storyblok/astro";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), mdx(), tailwind()],
-})
+  integrations: [compress(), mdx(), tailwind(), storyblok({
+    accessToken: process.env.STORYBLOK_ACCESS_TOKEN,
+    bridge: true,
+    componentsDir: 'src',
+    region: 'us',
+    components: {
+      page: 'storyblok/Page',
+      feature: 'storyblok/Feature',
+      grid: 'storyblok/Grid',
+      teaser: 'storyblok/Teaser',
+      blogPost: 'storyblok/BlogPost',
+      blogPostList: 'storyblok/BlogPostList'
+    }
+  })],
+  output: 'server',
+  adapter: netlify()
+});
